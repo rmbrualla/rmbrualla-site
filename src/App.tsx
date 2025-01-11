7b6ed31fbcde3139e8d1d0b1b27a42aaad833d7c
Ricardo Martin Brualla <rmbrualla@google.com>
2025-01-10 17:07:53 -0800

    changes to run on github

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      </BrowserRouter basename="rmbrualla-site">
         <Routes>
           <Route path="/" element={<Index />} />
         </Routes>
