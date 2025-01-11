ef5548e269cad8b969b6ba2cd72bb00c6af1514c
Ricardo Martin Brualla <rmbrualla@google.com>
2025-01-10 17:24:10 -0800

    clean

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -22,16 +22,37 @@ const Index = () => {
                 I am a researcher interested in the intersection of AI and 3D, and excited about creating new products in that space.
               </p>
               <p className="text-lg text-secondary mb-4">
-                Previously, I worked at Google for eight years, most recently as a Senior Staff Research Scientist, leading and managing a team working on <a className="text-blue-100" href="https://blog.google/products/shopping/search-on-2022-shopping/">generating 3D assets</a> for all products in Google Search , using NeRFs, diffusion and video models. 
-                Before that, I worked for five years on 3D telepresence on <a className="text-blue-100" href="https://blog.google/technology/research/project-starline/">Project Starline</a>, from building the first end-to-end demos to leading the ML efforts for real-time novel view synthesis.
+                Previously, I worked at Google for eight years, most recently as a Senior Staff Research Scientist, 
+                leading and managing a team working on 
+                <a className="text-blue-400" href="https://blog.google/products/shopping/search-on-2022-shopping/">generating 3D assets</a>
+                 for all products in Google Search , using NeRFs, diffusion and video models. 
+                Before that, I worked for five years on 3D telepresence on
+                 <a className="text-blue-400" href="https://blog.google/technology/research/project-starline/">Project Starline</a>, 
+                 from building the first end-to-end demos to leading the ML efforts for real-time novel view synthesis.
               </p>
               <p className="text-lg text-secondary mb-4">
-                I obtained my PhD in Computer Science and Engineering at the <a className="text-blue-100" href="http://www.cs.washington.edu/">University of Washington</a> in 2016 with <a className="text-blue-100" href="http://homes.cs.washington.edu/~seitz/">Steve Seitz</a> as my advisor. Two of my PhD projects went viral online: <a className="text-blue-100" href="https://grail.cs.washington.edu/projects/timelapse/">timelapse mining from Internet photos</a>, and a <a className="text-blue-100" href="https://medium.com/hackernoon/seattle-3-year-time-lapse-video-from-the-space-needle-9a9e76cfe8bf">timelapse of Seattle from the Space Needle</a>. I hold two bachelor degrees in Math and Informatics from <a className="text-blue-100" href="https://www.upc.edu/en">Barcelona Tech / UPC</a>.
+                I obtained my PhD in Computer Science and Engineering at the 
+                <a className="text-blue-400" href="http://www.cs.washington.edu/">University of Washington</a> in 2016 with 
+                <a className="text-blue-400" href="http://homes.cs.washington.edu/~seitz/">Steve Seitz</a> as my advisor. Two of my PhD projects 
+                went viral online: 
+                <a className="text-blue-400" href="https://grail.cs.washington.edu/projects/timelapse/">timelapse mining from Internet photos</a>, and a 
+                <a className="text-blue-400" href="https://medium.com/hackernoon/seattle-3-year-time-lapse-video-from-the-space-needle-9a9e76cfe8bf">timelapse of Seattle from the Space Needle</a>. 
+                I hold two bachelor degrees in Math and Informatics from <a className="text-blue-400" href="https://www.upc.edu/en">Barcelona Tech / UPC</a>.
               </p>
               <p className="text-lg text-secondary mb-4">
-                Outside of work, I love the outdoors and adventures. During COVID-19, my wife and I crossed Alaska from Ketchikan to Kotzebue on foot, packraft and cycling. We wrote about it on our blog called <a className="text-blue-100" href="http://north2arctic.com">north2arctic</a>, and we were featured in the <a className="text-blue-100" href="https://issuu.com/dougc/docs/mtrvol118no3_summer24_web/22?experiment=previewReaderTestMode%2Cnew-bff-translate%2Cnew-bff-fullscreen%2Cnew-bff-video%2Cnew-bff-purchased">Mountaineers magazine</a>. 
+                Outside of work, I love the outdoors and adventures. During COVID-19, my wife and I crossed Alaska from Ketchikan to Kotzebue on
+                 foot, packraft and cycling. We wrote about it on our blog called <a className="text-blue-400" href="http://north2arctic.com">north2arctic</a>,
+                 and we were featured in the 
+                 <a className="text-blue-400" href="https://issuu.com/dougc/docs/mtrvol118no3_summer24_web/22?experiment=previewReaderTestMode%2Cnew-bff-translate%2Cnew-bff-fullscreen%2Cnew-bff-video%2Cnew-bff-purchased">Mountaineers magazine</a>. 
               </p>
-
+          </div>
+          <div className="prose prose-md">
+            <p className="text-lg text-secondary mb-4">
+                <a className="text-blue-400" href="mailto:rmbrualla@gmail.com">Email</a> &nbsp/&nbsp
+                <a className="text-blue-400" href="https://scholar.google.com/citations?hl=en&user=9F59OCYAAAAJ">Google Scholar</a> &nbsp/&nbsp
+                <a className="text-blue-400" href="https://twitter.com/rmbrualla">Twitter</a> &nbsp/&nbsp
+                <a className="text-blue-400" href="https://github.com/rmbrualla/">Github</a>
+              </p>
           </div>
         </section>
 
